0f8300d883869d948ecabad3384771c0703db97b
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-03 22:41:04 -0300

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -5,4 +5,37 @@ sum(28, 27)
 
 */
 
+/* ARROW FUNCTION 
+const sayMyName = (name) => {
+    console.log(name)
+}
 
+sayMyName('Felipe')
+*/
+
+
+// CALLBACK FUNCTION
+/*
+function sayMyName(name){
+    console.log('estou dentro da function')
+    name()
+}
+
+sayMyName(
+    () => {
+        console.log('executando uma callback')
+    }
+)
+*/
+
+//FUNCTION CONSTRUCT
+function Person(name){
+    this.name = name
+    
+    this.walk = function () {
+        return this.name + ' this walking'
+    }
+}
+
+let person1 = new Person('Felipe')
+console.log(person1.walk())
