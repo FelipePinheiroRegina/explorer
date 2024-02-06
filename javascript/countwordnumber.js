f65e1c7fc69bbee0a9e23ad259b9bd867830195e
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-06 20:42:27 -0300

    .

--- javascript/countwordnumber.js
+++ javascript/countwordnumber.js
@@ -2,5 +2,6 @@
 let word = 'pindamanhangaba';
 console.log(word.length)
 
+// for count a variable type of number. Convert number for  string and after use .lenght
 let numbers = 1234;
 console.log(String(numbers).length)
---
+++ javascript/decimals.js
@@ -0,0 +1,4 @@
+let number = 123.123;
+
+console.log(number.toFixed(2).replace('.' , ',')) //defined how much decimal and change . by ,
+
