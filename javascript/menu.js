b565ca9f355111ac96f32422ff56aae3622e63df
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-10 19:07:03 -0300

    .

---
+++ javascript/menu.js
@@ -0,0 +1,40 @@
+/*
+Criar um menu onde tenha três opções; 1 cadastrar itens; 2 ver itens, se nao tiver nada, mostrar mensagem 'nao existe itens cadastrados'; 3 encerrar menu
+*/
+
+let option
+let list = new Array()
+
+while (option != 3) {
+    option = Number(prompt(`
+    Opções do menu
+
+    1. cadastrar itens na lista
+    2. ver itens na lista
+    3. encerrar menu
+    `))
+
+    if (option == 1) {
+        let cadMore 
+        while (cadMore != 'N') {
+            list.push(prompt('Nome do item: '))
+            cadMore = prompt('Deseja cadastrar mais itens? [ S / N ]: ').toUpperCase()
+            
+        }
+    }
+    else if (option == 2) {
+        if (list.length == 0) {
+            alert('Não existe itens cadastrados na lista')
+        } else {
+            let listOrganization = list.join(', ')
+            alert(listOrganization)
+        }
+        
+    }
+    else if (option == 3) {
+        alert('Good bye, see you later!')
+    } 
+    else {
+        alert('Opção inválida. Tente outra vez!')
+    }
+}
