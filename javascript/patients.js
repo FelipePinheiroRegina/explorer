15ef2c2ccd4f404a1d2a8f845b6eaca000952cc9
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-11 13:19:48 -0300

    .

--- javascript/patients.js
+++ javascript/patients.js
@@ -1,32 +1,67 @@
-const patients = [
-    // patients one
-    {
-        name: 'Felipe Pinheiro',
-        age: 25,
-        weight: 83.8,
-        height: 1.81
-    },
-    //patients two
-    {
-        name: 'Renato Cariani',
-        age: 42,
-        weight: 100.8,
-        height: 1.91
-    },
-    //patients three
-    {
-        name: 'Gustavo Guanabara',
-        age: 39,
-        weight: 100.1,
-        height: 1.95
-    },
-]
+let patients = []
+let option
 
-for (let patient of patients) {
-    alert(`
-        Nome do paciente: ${patient.name}
-        Idade do paciente: ${patient.age}
-        Peso do paciente: ${patient.weight.toFixed(3) + ' kg'}
-        Altura do paciente: ${patient.height + ' cm'}
-    `)    
+
+const IMC = (weight, height) => {
+    let imc = weight / (height ** 2)
+    return imc.toFixed(2)
+}
+
+while (option != 3) {
+    option = Number(prompt(`
+    Olá, bem vindo ao menu de pacientes.
+    Hospital: Viva lá vida
+
+    [ 1 ] Mostrar pacientes
+    [ 2 ] Cadastrar pacientes
+    [ 3 ] Encerrar
+    `))
+
+    switch (option) {
+        
+        case 1:
+            if (patients.length == 0) {
+                alert('Não há nenhum paciente cadastrado!')
+            } 
+            else {
+                for (let patient of patients) {
+                    alert(`
+                        Nome do paciente: ${patient.name}
+                        Idade do paciente: ${patient.age}
+                        Peso do paciente: ${patient.weight.toFixed(3) + ' kg'}
+                        Altura do paciente: ${patient.height + ' cm'}
+                    `)    
+                }
+                let verImc = prompt('Deseja ver o IMC de qual paciente? ')
+                
+                for (let patient of patients) {
+                    if(verImc.toLowerCase() == patient.name.toLowerCase()){
+                        alert(`O ${patient.name}, tem o IMC de: ${IMC(patient.weight, patient.height)}`)
+                    }
+                }
+                
+            }
+            break
+        
+        case 2:
+            let cadMore
+            while (cadMore != 'n') {
+                patients.push({
+                    name: prompt('Nome do paciente: '),
+                    age: Number(prompt('Idade do paciente: ')),
+                    weight: Number(prompt('Peso do paciente, Ex.: 100.1 kg: ')),
+                    height: Number(prompt('Altura do paciente, Ex.: 1.90 cm: '))
+                })
+                cadMore = prompt('Deseja cadastrar mais pacientes, [ s ] / [ n ]: ').toLowerCase()
+            }
+            break
+        
+        case 3:
+            alert('Encerrando programa de pacientes...')
+            option = 3
+            break
+
+        default:
+            alert('ERRO. TENTE UMA OPÇÃO VALIDA.')    
+    }
 }
