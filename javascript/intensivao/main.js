7aa6eb90509dc8adc38f95128369e35cbfc6383a
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-13 13:12:50 -0300

    desafio

--- javascript/intensivao/main.js
+++ javascript/intensivao/main.js
@@ -11,21 +11,125 @@ function Somar() {
     number1 = Number(number1.value)
     number2 = Number(number2.value)
 
-    answer.innerText = number1 + number2
+    answer.innerText = `${number1} + ${number2} é ${number1 + number2}`
 }
 
 function enumber() {
-    let enumb = document.getElementById('idenumb')
+    let isnumb = document.getElementById('idenumb')
 
-    enumb = Number(enumb.value)
-    answer.innerHTML = typeof enumb
-    if (enumb === Number) {
-        answer.innerHTML = 'é um número'
+    isnumb = isnumb.value
+
+    let isNumber
+
+    for (let i = 1; i <= 9; i++) {
+        if(isnumb.includes(i)){
+            isNumber = `${isnumb} é um número`
+            break
+        }
+        else {
+            isNumber = `${isnumb} não é um número`
+        }
+    }
+    
+    answer.innerHTML = isNumber
+    
+}
+
+function isString(){
+    let stringOrNumber = document.getElementById('idstring')
+
+    stringOrNumber = stringOrNumber.value
+
+    let isString
+
+    for (let i = 1; i <= 9; i++) {
+        if (stringOrNumber.includes(i)){
+            isString = `${stringOrNumber} não é String`
+            break
+        }
+        else {
+            isString = `${stringOrNumber} é String`
+        }
+    }
+
+    answer.innerText = isString
+}
+
+function isBoolean() {
+    let isBoolean = document.getElementById('idboo')
+    isBoolean = isBoolean.value.toLowerCase()
+    
+    if (isBoolean.includes('true')){
+        isBoolean = 'Booleano'
+    }
+    else if (isBoolean.includes('false')){
+        isBoolean = 'Booleano'
     }
     else {
-        answer.innerHTML = 'não é um número'
+        isBoolean = 'Não é Booleano'
+    }
+
+    answer.innerText = isBoolean
+}
+
+function subtracao() {
+    let numberOne = document.getElementById('idnumber1')
+    let numberTwo = document.getElementById('idnumber2')
+
+    numberOne = Number(numberOne.value)
+    numberTwo = Number(numberTwo.value)
+
+    answer.innerText = `${numberOne} - ${numberTwo} é ${numberOne - numberTwo}`
+}
+
+function multiplicar() {
+    let numberOne = document.getElementById('idmult1')
+    let numberTwo = document.getElementById('idmult2')
+
+    numberOne = Number(numberOne.value)
+    numberTwo = Number(numberTwo.value)
+
+    answer.innerText = `${numberOne} x ${numberTwo} é ${numberOne * numberTwo}`
+}
+
+function dividir() {
+    let numberOne = document.getElementById('iddiv1')
+    let numberTwo = document.getElementById('iddiv2')
+
+    numberOne = Number(numberOne.value)
+    numberTwo = Number(numberTwo.value)
+
+    answer.innerText = `${numberOne} / ${numberTwo} é ${numberOne / numberTwo}`
+    
+}
+
+function par() {
+    let isPar = document.getElementById('idpar')
+
+    isPar = Number(isPar.value)
+
+    if (isPar % 2 == 0) {
+        answer.innerText = `${isPar} é Par`
+    }
+    else {
+        answer.innerText = `${isPar} não é Par`
     }
 }
+
+function impar() {
+    let isImpar = document.getElementById('idimpar')
+
+    isImpar = Number(isImpar.value)
+
+    if (isImpar % 2 == 1) {
+        answer.innerText = `${isImpar} é Impar`
+    }
+    else {
+        answer.innerText = `${isImpar} não é Impar`
+    }
+}
+
+answer.innerText = 'Bem - Vindo'
 
 
 
