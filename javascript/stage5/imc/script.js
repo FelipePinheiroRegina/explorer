8e4f6c83961affc229d4b4ca388feec887e8d543
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-02-20 22:48:43 -0300

    .

--- javascript/stage5/imc/script.js
+++ javascript/stage5/imc/script.js
@@ -1,18 +1,47 @@
 // Variables
-const weight = document.querySelector('#weight')
-const height = document.querySelector('#height')
 const form = document.querySelector('form')
+let weight = document.querySelector('#weight')
+let height = document.querySelector('#height')
+
+const Modal = {
+    // Variables modal 
+    screenModal: document.querySelector('.modal'), 
+    exitModal: document.querySelector('.x'),
+    text_imc: document.querySelector('.yourImc'),
+
+    // Methods modal
+    open() {
+        this.screenModal.classList.add('open')
+    },
+    close(){
+        this.screenModal.classList.remove('open')
+    }
+}
 
 // Events
 form.onsubmit = event => {
-    event.preventDeafult()
+    event.preventDefault()
+    
+    weight = weight.value
+    height = height.value
+    
+    const imc = IMC(weight, height)
+    const message = `Seu IMC é de ${imc.toFixed(2)}`
 
+    Modal.text_imc.innerHTML = message
+    Modal.open()
 }
+
+Modal.exitModal.onclick = () => Modal.close()
+
 
 // Functions
-function IMC(weight, height){
-    let imc = height ** 2 / weight
-    return imc
+const IMC = (weight, height) => {
+    let yourImc = weight / ((height / 100) ** 2)
+    return yourImc
 }
 
-console.log(80 / 181 ** 2)
+
+
+
+
