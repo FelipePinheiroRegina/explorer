4c55520a84b60221e5ab6b626c4264b3f7abf0a4
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-03-03 19:42:35 -0300

    .

--- javascript/stage5/extratimer/js/buttonssounds.js
+++ javascript/stage5/extratimer/js/buttonssounds.js
@@ -5,58 +5,8 @@ import { state } from './state.js'
 buttons_sounds.addEventListener('click', (event) => {
     let requiredSoundBackground = event.target.dataset.sound
     console.log(requiredSoundBackground)
-
-    switch(requiredSoundBackground){
-        case 'jungle':
-            document.querySelector('.ph-tree').classList.toggle('button-clicked')
-            
-            state.Mute = document.documentElement.classList.toggle('music-on')
-            
-            if(state.Mute){
-                soundJungle.play()
-                return;
-            }
-
-           soundJungle.pause()
-           break;
-        
-        case 'rain':
-            document.querySelector('.ph-cloud-rain').classList.toggle('button-clicked')
-            
-            state.Mute = document.documentElement.classList.toggle('music-on')
-            
-            if(state.Mute){
-                soundRain.play()
-                return;
-            }
-
-            soundRain.pause()
-            break;
-
-        case 'store':
-            document.querySelector('.ph-storefront').classList.toggle('button-clicked')
-            
-            state.Mute = document.documentElement.classList.toggle('music-on')
-            
-            if(state.Mute){
-                soundStore.play()
-                return;
-            }
-
-            soundStore.pause()
-            break;
-    
-        case 'fire':
-            document.querySelector('.ph-campfire').classList.toggle('button-clicked')
-            
-            state.Mute = document.documentElement.classList.toggle('music-on')
-            
-            if(state.Mute){
-                soundFire.play()
-                return;
-            }
-
-            soundFire.pause()
-            break;
-    }
+    document.querySelector('.ph-tree').classList.toggle('button-clicked')
+    document.querySelector('.ph-cloud-rain').classList.toggle('button-clicked')
+    document.querySelector('.ph-storefront').classList.toggle('button-clicked')
+    document.querySelector('.ph-campfire').classList.toggle('button-clicked')
 })
