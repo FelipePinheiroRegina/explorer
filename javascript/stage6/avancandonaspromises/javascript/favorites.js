4b9f87e14f281f77b8ceb8b24cec2dcd75ae272f
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-03-15 22:37:36 -0300

    avançando no projeto github favorites

--- javascript/stage6/avancandonaspromises/javascript/favorites.js
+++ javascript/stage6/avancandonaspromises/javascript/favorites.js
@@ -1,23 +1,28 @@
+export class githubUsers{
+    static search(username) {  
+        const endpoint = `http://api.github.com/users/${username}`
+
+        return fetch(endpoint)
+        .then(data => data.json())
+        .then(({login, name, public_repos, followers}) => ({login, name, public_repos, followers}))
+    }
+}
+
 export class Favorites {
     constructor(root) {
         this.root = document.querySelector(root)
         this.load()
+
+        githubUsers.search('felipe-pinheiro98').then(user => console.log(user))
     }
 
     load(){
-        this.entries = [{
-            login: 'felipe-pinheiro98',
-            name: 'Felipe Pinheiro',
-            public_repos: '76',
-            fallowers: '12000'
-        },
-        {
-            login: 'RichardPinheiro',
-            name: 'Richard Pinheiro',
-            public_repos: '89',
-            fallowers: '120500'
-        }
-        ]
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+    }
+
+    async add(username) {
+        const user = await githubUsers.search(username)
+        console.log(user)
     }
 
     delete(user){
@@ -33,6 +38,17 @@ export class FavoritesView extends Favorites{
         super(root)
         this.tbody = this.root.querySelector('table tbody')
         this.update()
+        this.onadd()
+    }
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+        }
     }
 
     update() {
@@ -46,7 +62,7 @@ export class FavoritesView extends Favorites{
            row.querySelector('.users p').textContent = user.name
            row.querySelector('.users span').textContent = user.login
            row.querySelector('.repositories').textContent = user.public_repos 
-           row.querySelector('.fallowers').textContent = user.fallowers
+           row.querySelector('.followers').textContent = user.followers
 
            row.querySelector('.times').onclick = () => {
             const isTrue = confirm('Deseja excluir esse usuário?')
