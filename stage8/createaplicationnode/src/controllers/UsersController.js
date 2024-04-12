416da365b4fab9d2bf762e15f3cd173bcd232419
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-04-11 22:52:05 -0300

    method put

--- stage8/createaplicationnode/src/controllers/UsersController.js
+++ stage8/createaplicationnode/src/controllers/UsersController.js
@@ -1,5 +1,6 @@
 const appError = require('../utils/appError')
 const sqliteConnection = require('../database/sqlite')
+const { hash, compare } = require('bcryptjs')
 
 
 class UsersController {
@@ -14,9 +15,58 @@ class UsersController {
             throw new appError('This e-mail already exists')
         }
 
-        await database.run('INSERT INTO users(name, email, password) VALUES(?, ?, ?)', [name, email, pass])
+        const hashpassword = await hash(pass, 8)
+
+        await database.run('INSERT INTO users(name, email, password) VALUES(?, ?, ?)', [name, email, hashpassword])
 
         return response.status(201).json()
+    }
+
+    async update(request, response) {
+        const { name, email, oldpassword, newpassword} = request.body
+        const { id } = request.params
+
+        const database = await sqliteConnection()
+
+        const user = await database.get('SELECT * FROM users WHERE id = (?)', [id])
+
+        if(!user) {
+            throw new appError('User not found')
+        }
+
+        const userWithUpdateEmail = await database.get('SELECT * FROM users WHERE email = (?)', [email])
+
+        if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
+            throw new appError('This E-mail already exists')
+        }
+
+        user.name  = name ?? user.name
+        user.email = email ?? user.email
+
+        if(newpassword && !oldpassword){
+            throw new appError('You need to enter your old password');
+        }
+
+        if(newpassword && oldpassword){
+            const checkOldpassword = await compare(oldpassword, user.password)
+
+            if(!checkOldpassword){
+                throw new appError('Password invalid')
+            }
+
+            user.password = await hash(newpassword, 8)
+        }
+        
+        await database.run(`
+            UPDATE users SET
+            name = ?,
+            email = ?,
+            password = ?,
+            updated_at = DATETIME('now','localtime')
+            WHERE id = (?)
+        `, [user.name, user.email, user.password, id])
+
+        return response.status(200).json()
     }
 }
 
