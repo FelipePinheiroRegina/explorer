73645b03380cb1c4319228c9642447b49368e028
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-04-17 23:08:43 -0300

    conection db

--- stage8/rocketmoveis/src/controllers/UserController.js
+++ stage8/rocketmoveis/src/controllers/UserController.js
@@ -1,6 +1,12 @@
+const appError = require("../utils/appError")
+
 class UserController{
     create(req, res){
         const { name, email, password } = req.body
+
+        if(!name){
+            throw new appError('name required')
+        }
 
         return res.json({name, email, password})
     }
--- stage8/rocketmoveis/src/server.js
+++ stage8/rocketmoveis/src/server.js
@@ -1,12 +1,34 @@
+require('express-async-errors')
+const migrationRun = require('./database/sqlite/migration/index')
 const express = require('express')
 const routes = require("./routes")
+const appError = require('./utils/appError')
+
 
 const app = express()
 
 app.use(express.json())
 
+migrationRun()
+
 app.use(routes)
 
+
+app.use((error, request, response, next) => {
+    if(error instanceof appError){
+        return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message
+        })
+    }
+
+    console.log(error)
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal error server'
+    })
+})
 
 const PORT = 3000
 app.listen(PORT, console.log(`I'm listening on port ${PORT}`))
