4e9137f50a267c65f34c87f8a1a293d80628d4c2
Felipe Pinheiro <flppinheiro@hotmail.com>
2024-04-21 22:02:57 -0300

    styled components and react

---
+++ stage9/rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,21 @@
+import { Container, Links } from './styles'
+import { Button } from '../../components/Button'
+import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
+
+export default function Details(){
+  return(
+   <Container>
+      <Header />
+      
+      <Section title="Links úteis">
+        <Links>
+          <li><a href="#">https://rocketseat.com.br/</a></li>
+          <li><a href="#">https://rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
+      <Button title="Back"/>
+   </Container>
+  )
+}
